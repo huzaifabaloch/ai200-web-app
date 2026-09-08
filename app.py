0b23b283501d092d254a9c2947f6c59b270b7c54
Huzaifa Baloch <35249759+huzaifabaloch@users.noreply.github.com>
2026-09-08 23:55:02 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def home():
     <!DOCTYPE html>
     <html>
     <head>
-        <title>Web App</title>
+        <title>Web App 2.0</title>
         <style>
             body {
                 font-family: Arial, sans-serif;
